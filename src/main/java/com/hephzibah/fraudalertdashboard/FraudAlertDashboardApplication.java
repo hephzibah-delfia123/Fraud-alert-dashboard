ed457cfb011257d97f98eb0dfbfd00eed2b4d45d
hephzibah-delfia123 <80846497+hephzibah-delfia123@users.noreply.github.com>
2022-03-25 02:08:46 +0530

    Fraud Alert Dashboard

---
+++ src/main/java/com/hephzibah/fraudalertdashboard/FraudAlertDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.hephzibah.fraudalertdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication
+public class FraudAlertDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAlertDashboardApplication.class, args);
+	}
+}
+
+*/
